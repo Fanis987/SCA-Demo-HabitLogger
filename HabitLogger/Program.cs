f49764f61e6d6df5f9eafdf08a60e703640a77a6
fguy987 <126913238+fguy987@users.noreply.github.com>
2024-09-08 11:11:30 +0200

    Added a connection to a db and a drinking water table for testing

--- HabitLogger/Program.cs
+++ HabitLogger/Program.cs
@@ -1,3 +1,56 @@
 ﻿//MAIN PROGRAM
 
 using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+
+//Get the conenction string from a json file
+var configBuilder = new ConfigurationBuilder();
+
+configBuilder.AddJsonFile("appSettingsExtra.json",optional:false,reloadOnChange:false);
+
+var config = configBuilder.Build();
+
+var connectionString = config["connectionString"];
+
+//Create a connection to the database
+try
+{
+    using (var connection = new SqliteConnection(connectionString) )
+    {
+        connection.Open();
+        var command = connection.CreateCommand();
+        command.CommandText = "";
+        command.ExecuteNonQuery();
+        connection.Close();
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occured while connecting to the database:");
+    Console.WriteLine(ex.Message);
+}
+
+//create a table in the database
+try
+{
+    using (var connection = new SqliteConnection(connectionString))
+    {
+        connection.Open();
+        var command = connection.CreateCommand();
+        command.CommandText = @"
+                        CREATE TABLE IF NOT EXISTS DrinkingWater ( 
+                        Id INTEGER PRIMARY KEY,
+                        Date DA NOT NULL
+                        )";
+        command.ExecuteNonQuery();
+        //closes automatically
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occured while trying to create a table in the database:");
+    Console.WriteLine(ex.Message);
+}
+
+
+
